e8878231d87ca1d3507552018c299a53a3814582
Louis <dussoulier.louis@hotmai.fr>
2021-03-09 19:12:46 +0100

    Group info + student info

--- app/src/main/java/fr/epsi/atelier/MainActivity.java
+++ app/src/main/java/fr/epsi/atelier/MainActivity.java
@@ -2,13 +2,38 @@ package fr.epsi.atelier;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AtelierActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.buttonEtudiant1).setOnClickListener(this);
+        findViewById(R.id.buttonEtudiant2).setOnClickListener(this);
+        findViewById(R.id.buttonEtudiant3).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.buttonEtudiant1:
+                Studentinfo.displayActivity(this,"Louis","Etudiant");
+                break;
+            case R.id.buttonEtudiant2:
+                Studentinfo.displayActivity(this,"Marien","Etudiant");
+                break;
+            case R.id.buttonEtudiant3:
+                Studentinfo.displayActivity(this,"Antoine","Etudiant");
+                break;
+        }
     }
 }
+
+
+
+
